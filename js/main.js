b302324a8f58e429198e3884854c20ae68e0dcd1
Robbert <robbertluit@hotmail.com>
2017-12-09 02:49:20 +0100

    updated css / incl jquery

--- js/main.js
+++ js/main.js
@@ -1,57 +1,68 @@
-document.onkeydown = checkKey;
+    document.onkeydown = checkKey;
+$( document ).ready(function() {
+ window.onload = choosePic;
 
-function checkKey(e) {
+    // var myPix = new Array("/images/lion.jpg","images/tiger.jpg","images/bear.jpg");
+    //
+    // function choosePic() {
+    //     var randomNum = Math.floor(Math.random() * myPix.length);
+    //     document.getElementById("myPicture").src = myPix[randomNum];
+    // }
+    //
+    // function getRandomImage(imgAr, path) {
+    //     path = path || 'design/elementen/images/'; // default path here
+    //     var num = Math.floor( Math.random() * imgAr.length );
+    //     var img = imgAr[ num ];
+    //     var imgStr = '<img src="' + path + img + '" alt = "">';
+    //     document.write(imgStr); document.close();
+    // }
 
-    e = e || window.event;
+    function testRandomImage() {
 
-    if (e.keyCode == '38') {
-        console.log("up arrow pressed");
-    }
-    else if (e.keyCode == '40') {
-        console.log("down arrow pressed");
-    }
-    else if (e.keyCode == '37') {
-        console.log("left arrow pressed");
-    }
-    else if (e.keyCode == '39') {
-        console.log("right arrow pressed");
-    }
-    else if (e.keyCode == 87)
-    {
-        console.log("w key pressed")
-    }
-    else if (e.keyCode == 65)
-    {
-        console.log("a key pressed")
-    }
-    else if (e.keyCode == 83)
-    {
-        // START BUTTON
-        console.log("s key pressed")
-        location.href = 'pijlen.html';
-    }
-}
+    var path = '../design/elementen/images/',
+        imgs = ['1.png','2.png','3.png'],
+        i = Math.floor(Math.random()*imgs.length);
+    $('.el').append("<img src='"+path+imgs[i]+"'>").hide().fadeIn(2000);
 
-// window.onload = choosePic;
+    }
 
-// var myPix = new Array("/images/lion.jpg","images/tiger.jpg","images/bear.jpg");
-//
-// function choosePic() {
-//     var randomNum = Math.floor(Math.random() * myPix.length);
-//     document.getElementById("myPicture").src = myPix[randomNum];
-// }
-//
-// function getRandomImage(imgAr, path) {
-//     path = path || 'design/elementen/images/'; // default path here
-//     var num = Math.floor( Math.random() * imgAr.length );
-//     var img = imgAr[ num ];
-//     var imgStr = '<img src="' + path + img + '" alt = "">';
-//     document.write(imgStr); document.close();
-// }
+});
 
+    function checkKey(e) {
 
-var path = '../design/elementen/images/',
-    imgs = ['1.png','2.png','3.png'],
-    i = Math.floor(Math.random()*imgs.length);
-$('.el').append("<img src='"+path+imgs[i]+"'>").hide().fadeIn(2000);
-}
+        e = e || window.event;
+
+        if (e.keyCode == '38') {
+            console.log("up arrow pressed");
+        }
+        else if (e.keyCode == '40') {
+            // DOWN
+            console.log("down arrow pressed");
+        }
+        else if (e.keyCode == '37') {
+            // LEFT
+            console.log("left arrow pressed");
+        }
+        else if (e.keyCode == '39') {
+            // RIGHT
+            console.log("right arrow pressed");
+        }
+        else if (e.keyCode == 87)
+        {
+            // BLAUW
+            console.log("w key pressed")
+        }
+        else if (e.keyCode == 65)
+        {
+            // ROOD
+            console.log("a key pressed")
+        }
+        else if (e.keyCode == 83)
+        {
+            // START BUTTON
+            console.log("s key pressed");
+            location.href = 'pijlen.html';
+        }
+    }
+
+    //
