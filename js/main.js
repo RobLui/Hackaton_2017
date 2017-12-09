68f9a95919f68c93596cbdc9879c0a480e612255
Robbert <robbertluit@hotmail.com>
2017-12-09 07:53:56 +0100

    s

--- js/main.js
+++ js/main.js
@@ -1,11 +1,87 @@
 $( document ).ready(function() {
 
+    var	clsStopwatch = function() {
+        var	startAt	= 0;	// Time of last start / resume. (0 if not running)
+        var	lapTime	= 0;	// Time on the clock when last stopped in milliseconds
+
+        var	now	= function() {
+            return (new Date()).getTime();
+        };
+
+        this.start = function() {
+            startAt	= startAt ? startAt : now();
+        };
+
+        this.stop = function() {
+            lapTime	= startAt ? lapTime + now() - startAt : lapTime;
+            startAt	= 0; // Paused
+        };
+
+        this.reset = function() {
+            lapTime = startAt = 0;
+        };
+
+        this.time = function() {
+            return lapTime + (startAt ? now() - startAt : 0);
+        };
+    };
+
+    var x = new clsStopwatch();
+    var $time;
+    var clocktimer;
+
+    function pad(num, size) {
+        var s = "0000" + num;
+        return s.substr(s.length - size);
+    }
+
+    function formatTime(time) {
+        var h = m = s = ms = 0;
+        var newTime = '';
+
+        h = Math.floor( time / (60 * 60 * 1000) );
+        time = time % (60 * 60 * 1000);
+        m = Math.floor( time / (60 * 1000) );
+        time = time % (60 * 1000);
+        s = Math.floor( time / 1000 );
+        ms = time % 1000;
+
+        newTime = pad(h, 2) + ':' + pad(m, 2) + ':' + pad(s, 2) + ':' + pad(ms, 3);
+        return newTime;
+    }
+
+    function show() {
+        $time = document.getElementById('time');
+        update();
+    }
+
+    function update() {
+        $time.innerHTML = formatTime(x.time());
+    }
+
+    function start() {
+        clocktimer = setInterval("update()", 1);
+        x.start();
+    }
+
+    function stop() {
+        x.stop();
+        clearInterval(clocktimer);
+    }
+
+    function reset() {
+        stop();
+        x.reset();
+        update();
+    }
+
     var arr = ["background-red", "background-blue"];
     var audio = document.getElementById("audio");
 
     function playAudio() {
         audio.play();
     }
+
     function testRandomImage() {
 
         var path = 'design/elementen/images/',
@@ -36,14 +112,13 @@ $( document ).ready(function() {
 
         var imgs = Math.random() <= 0.3 ? arrows : people;
         var i = Math.floor(Math.random()*imgs.length);
-
         $('.imageclass').append("<img src='"+path+imgs[i]+"'>").hide().fadeIn(2000);
     }
-    $("#count_num").delay(1000).queue(function() {
-        $('#count_num').html("2").delay(1000).queue(function() {
-            $('#count_num').html("1")
-        });
-    });
+    // $("#count_num").delay(1000).queue(function() {
+    //     $('#count_num').html("2").delay(1000).queue(function() {
+    //         $('#count_num').html("1")
+    //     });
+    // });
 
     window.setInterval(function(){
         $('.imageclass img').remove();
@@ -88,7 +163,6 @@ function checkKey(e) {
     }
     else if (e.keyCode == 83)
     {
-        // START BUTTON
         console.log("s key pressed");
 
         if(window.location.href.indexOf("index.html") > -1) {
